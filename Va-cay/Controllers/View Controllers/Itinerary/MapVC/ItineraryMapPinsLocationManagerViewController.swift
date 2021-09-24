645b6375233517154b32b85648526cfec4685fd3
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-09-23 17:26:03 -0700

    changed background color of maps back to customOffWhite

--- Va-cay/Controllers/View Controllers/Itinerary/MapVC/ItineraryMapPinsLocationManagerViewController.swift
+++ Va-cay/Controllers/View Controllers/Itinerary/MapVC/ItineraryMapPinsLocationManagerViewController.swift
@@ -51,7 +51,7 @@ class ItineraryMapPinsLocationManagerViewController: UIViewController {
         let searchBar = resultSearchController!.searchBar
         searchBar.sizeToFit()
         searchBar.placeholder = "Where to?"
-        searchBar.backgroundColor = Colors.mapBackgroundColor
+//        searchBar.backgroundColor = Colors.mapBackgroundColor
         navigationItem.searchController = resultSearchController
         
         resultSearchController?.hidesNavigationBarDuringPresentation = false
